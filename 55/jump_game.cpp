4160fc1763e3f5e890f1214e47b342aa6a02274f
xela6134 <leol3o05@gmail.com>
2024-09-05 12:50:42 +0900

    Added solution for jump game

---
+++ 55/jump_game.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+bool canJump(std::vector<int>& nums) {
+    int farthest = 0;
+
+    for (int i = 0; i < nums.size(); ++i) {
+        if (i > farthest) {
+            return false;
+        }
+
+        farthest = std::max(farthest, i + nums[i]);
+    }
+
+    return true;
+}
+
+auto main() -> int {
+    auto nums1 = std::vector<int>{2, 3, 1, 1, 4};
+    auto nums2 = std::vector<int>{3, 2, 1, 0, 4};
+    auto nums3 = std::vector<int>{8,2,4,4,4,9,5,2,5,8};
+
+    std::cout << std::boolalpha << canJump(nums1) << std::endl;
+    std::cout << std::boolalpha << canJump(nums2) << std::endl;
+    std::cout << std::boolalpha << canJump(nums3) << std::endl;
+}
