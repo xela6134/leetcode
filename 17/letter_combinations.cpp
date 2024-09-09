01484dc6106c6b292c3ecf21e60c5290fed9488d
xela6134 <leol3o05@gmail.com>
2024-09-09 14:52:26 +0900

    Solution for telephone number combinations

--- 17/letter_combinations.cpp
+++ 17/letter_combinations.cpp
@@ -1,27 +1,56 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <iostream>
+
+auto backtrack(
+    std::string curr, 
+    std::string remaining, 
+    std::unordered_map<char, std::string>& letters, 
+    std::vector<std::string>& result) -> void {
+
+    if (remaining == "") {
+        result.push_back(curr);
+        return;
+    }
+
+    std::string curr_chars = letters[remaining[0]];
+    std::string new_remaining = remaining.substr(1);
+
+    for (char& ch : curr_chars) {
+        backtrack(curr + ch, new_remaining, letters, result);
+    }
+}
 
 auto letterCombinations(std::string digits) -> std::vector<std::string> {
     if (digits.length() == 0) return {};
 
-    auto letter_map = std::unordered_map<char, std::vector<char>> {
-        {'2', {'a', 'b', 'c'}},
-        {'3', {'d', 'e', 'f'}},
-        {'4', {'g', 'h', 'i'}},
-        {'5', {'j', 'k', 'l'}},
-        {'6', {'m', 'n', 'o'}},
-        {'7', {'p', 'q', 'r', 's'}},
-        {'8', {'t', 'u', 'v'}},
-        {'9', {'w', 'x', 'y', 'z'}}
+    auto letters = std::unordered_map<char, std::string>{
+        {'2', "abc"},
+        {'3', "def"},
+        {'4', "ghi"},
+        {'5', "jkl"},
+        {'6', "mno"},
+        {'7', "pqrs"},
+        {'8', "tuv"},
+        {'9', "wxyz"},
     };
 
-    std::vector<std::string> result;
-    std::string current;
+    auto result = std::vector<std::string>{};
 
-    
+    backtrack("", digits, letters, result);
+
+    return result;
+}
+
+auto print_result(std::vector<std::string> result) -> void {
+    for (auto it = result.begin(); it != result.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
 }
 
 auto main() -> int {
-
+    auto digits1 = "23";
+    auto result1 = letterCombinations(digits1);
+    print_result(result1);
 }
