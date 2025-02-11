952b3f5d14f636d30c9aa297e5ae9eb1d83f3f36
xela6134 <leol3o05@gmail.com>
2025-02-11 18:09:33 +0900

    Some more revision

--- 14/longest_common_prefix.cpp
+++ 14/longest_common_prefix.cpp
@@ -3,17 +3,35 @@
 #include <algorithm>
 #include <iostream>
 
+// auto longestCommonPrefix(std::vector<std::string>& strs) -> std::string {
+//     if (strs.empty()) return "";
+
+//     std::string prefix = strs[0];
+
+//     for (int i = 1; i < strs.size(); ++i) {
+
+//         // start by comparing entire prefix and current str
+//         // cut down character by character until we have a match (index 0 represents this)
+
+//         while (strs[i].find(prefix) != 0) {
+//             prefix = prefix.substr(0, prefix.length() - 1);
+//             if (prefix == "") return "";
+//         }
+//     }
+
+//     return prefix;
+// }
+
 auto longestCommonPrefix(std::vector<std::string>& strs) -> std::string {
     if (strs.empty()) return "";
 
+    // Start with whole string, subtract characters from the back
     std::string prefix = strs[0];
 
     for (int i = 1; i < strs.size(); ++i) {
-
-        // start by comparing entire prefix and current str
-        // cut down character by character until we have a match (index 0 represents this)
-
         while (strs[i].find(prefix) != 0) {
+            // Returning 0 means that it is found at the start
+
             prefix = prefix.substr(0, prefix.length() - 1);
             if (prefix == "") return "";
         }
--- 17/letter_combinations.cpp
+++ 17/letter_combinations.cpp
@@ -3,22 +3,64 @@
 #include <unordered_map>
 #include <iostream>
 
-auto backtrack(
-    std::string curr, 
-    std::string remaining, 
-    std::unordered_map<char, std::string>& letters, 
-    std::vector<std::string>& result) -> void {
+// auto backtrack(
+//     std::string curr, 
+//     std::string remaining, 
+//     std::unordered_map<char, std::string>& letters, 
+//     std::vector<std::string>& result) -> void {
 
-    if (remaining == "") {
+//     if (remaining == "") {
+//         result.push_back(curr);
+//         return;
+//     }
+
+//     std::string curr_chars = letters[remaining[0]];
+//     std::string new_remaining = remaining.substr(1);
+
+//     for (char& ch : curr_chars) {
+//         backtrack(curr + ch, new_remaining, letters, result);
+//     }
+// }
+
+// auto letterCombinations(std::string digits) -> std::vector<std::string> {
+//     if (digits.length() == 0) return {};
+
+//     auto letters = std::unordered_map<char, std::string>{
+//         {'2', "abc"},
+//         {'3', "def"},
+//         {'4', "ghi"},
+//         {'5', "jkl"},
+//         {'6', "mno"},
+//         {'7', "pqrs"},
+//         {'8', "tuv"},
+//         {'9', "wxyz"},
+//     };
+
+//     auto result = std::vector<std::string>{};
+
+//     backtrack("", digits, letters, result);
+
+//     return result;
+// }
+
+void backtrack(std::vector<std::string>& result, std::unordered_map<char, std::string>& letters, std::string digits, std::string curr) {
+    // 1. Get the first number of the current digits string
+    // 2. Get the letters related to current string
+    // 3. Traverse each character through string, append to curr
+    // 4. Backtrack
+
+    if (digits == "") {
         result.push_back(curr);
         return;
     }
 
-    std::string curr_chars = letters[remaining[0]];
-    std::string new_remaining = remaining.substr(1);
+    char first_number = digits[0];
+    std::string letter_combination = letters[first_number];
 
-    for (char& ch : curr_chars) {
-        backtrack(curr + ch, new_remaining, letters, result);
+    for (char& ch : letter_combination) {
+        std::string new_curr = curr + ch;
+        std::string new_digits = digits.substr(1, digits.size() - 1);
+        backtrack(result, letters, new_digits, new_curr);
     }
 }
 
@@ -36,10 +78,8 @@ auto letterCombinations(std::string digits) -> std::vector<std::string> {
         {'9', "wxyz"},
     };
 
-    auto result = std::vector<std::string>{};
-
-    backtrack("", digits, letters, result);
-
+    std::vector<std::string> result;
+    backtrack(result, letters, digits, "");
     return result;
 }
 
