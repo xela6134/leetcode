63feee582d8fe56e1213b19ef91048eef2f66881
xela6134 <leol3o05@gmail.com>
2025-02-12 20:07:36 +0900

    More practice

--- 39/combination_sum.cpp
+++ 39/combination_sum.cpp
@@ -3,28 +3,58 @@
 #include <iostream>
 #include <algorithm>
 
-auto find_combinations(int start, int sum, int target, std::vector<int>& candidates, std::vector<int>& current, std::vector<std::vector<int>>& result) {
-    if (sum == target) {
-        result.push_back(current);
-        return;
-    }
+// auto find_combinations(int start, int sum, int target, std::vector<int>& candidates, std::vector<int>& current, std::vector<std::vector<int>>& result) {
+//     if (sum == target) {
+//         result.push_back(current);
+//         return;
+//     }
 
-    for (int i = start; i < candidates.size(); ++i) {
-        if (candidates[i] + sum > target) break;
+//     for (int i = start; i < candidates.size(); ++i) {
+//         if (candidates[i] + sum > target) break;
 
-        current.push_back(candidates[i]);
-        find_combinations(i, sum + candidates[i], target, candidates, current, result);
-        current.pop_back();
+//         current.push_back(candidates[i]);
+//         find_combinations(i, sum + candidates[i], target, candidates, current, result);
+//         current.pop_back();
+//     }
+// }
+
+// auto combinationSum(std::vector<int>& candidates, int target) -> std::vector<std::vector<int>> {
+//     std::vector<std::vector<int>> result;
+//     std::vector<int> current;
+
+//     std::sort(candidates.begin(), candidates.end());
+
+//     find_combinations(0, 0, target, candidates, current, result);
+
+//     return result;
+// }
+
+void backtrack(int index, int curr_sum, std::vector<int> curr_nums, int target, std::vector<std::vector<int>>& result, std::vector<int>& candidates) {
+    for (int i = index; i < candidates.size(); ++i) {
+        int candidate = candidates[i];
+        int new_sum = curr_sum + candidate;
+
+        std::vector<int> new_nums = curr_nums;
+        new_nums.push_back(candidate);
+
+        if (new_sum > target) {
+            return;
+        } else if (new_sum == target) {
+            std::sort(new_nums.begin(), new_nums.end());
+            result.push_back(new_nums);
+            return;
+        } else {
+            backtrack(i, new_sum, new_nums, target, result, candidates);
+        }
     }
 }
 
 auto combinationSum(std::vector<int>& candidates, int target) -> std::vector<std::vector<int>> {
     std::vector<std::vector<int>> result;
-    std::vector<int> current;
-
     std::sort(candidates.begin(), candidates.end());
 
-    find_combinations(0, 0, target, candidates, current, result);
+    // Variables Needed: index, current_sum, target, result, candidates
+    backtrack(0, 0, {}, target, result, candidates);
 
     return result;
 }
--- 48/rotate_image.cpp
+++ 48/rotate_image.cpp
@@ -1,29 +1,50 @@
 #include <vector>
 #include <iostream>
 
+// void rotate(std::vector<std::vector<int>>& matrix) {
+//     // if
+//     // 1 2 3
+//     // 4 5 6
+//     // 7 8 9
+
+//     // read 1 -> 2 -> 3
+//     // then paste it as
+//     // ? ? 1      ? 4 1
+//     // ? ? 2  ->  ? 5 2
+//     // ? ? 3      ? 6 3
+
+//     int size = static_cast<int>(matrix.size());
+
+//     std::vector<std::vector<int>> updated(size, std::vector<int>(size, 0));
+
+//     for (int i = 0; i < size; ++i) {
+//         for (int j = 0; j < size; ++j) {
+//             updated[j][size - 1 - i] = matrix[i][j];
+//         }
+//     }
+
+//     matrix = updated;
+// }
+
 void rotate(std::vector<std::vector<int>>& matrix) {
-    // if
-    // 1 2 3
-    // 4 5 6
-    // 7 8 9
+    int size = matrix.size();
+    std::vector<std::vector<int>> new_matrix(size, std::vector<int>(size, 0));
 
-    // read 1 -> 2 -> 3
-    // then paste it as
-    // ? ? 1      ? 4 1
-    // ? ? 2  ->  ? 5 2
-    // ? ? 3      ? 6 3
+    // (0,0) (0,1) (0,2)
+    // (1,0) (1,1) (1,2)
+    // (2,0) (2,1) (2,2)
 
-    int size = static_cast<int>(matrix.size());
-
-    std::vector<std::vector<int>> updated(size, std::vector<int>(size, 0));
+    // (2,0) (1,0) (0,0)
+    // (2,1) (1,1) (0,1)
+    // (2,2) (1,2) (0,2)
 
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
-            updated[j][size - 1 - i] = matrix[i][j];
+            new_matrix[j][size - 1 - i] = matrix[i][j];
         }
     }
 
-    matrix = updated;
+    matrix = new_matrix;
 }
 
 void print_matrix(std::vector<std::vector<int>>& matrix) {
@@ -37,7 +58,7 @@ void print_matrix(std::vector<std::vector<int>>& matrix) {
 }
 
 auto main() -> int {
-    std::vector<std::vector<int>> matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    std::vector<std::vector<int>> matrix1 = {{1,2,3}, {4,5,6}, {7,8,9}};
     std::vector<std::vector<int>> matrix2 = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
     rotate(matrix1);
     rotate(matrix2);    
