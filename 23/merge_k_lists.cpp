7f1b0fa23859cf27233912b63c545f8939175e36
xela6134 <leol3o05@gmail.com>
2025-03-06 17:22:28 +1100

    Edited solution, revision for Merge K Lists

--- 23/merge_k_lists.cpp
+++ 23/merge_k_lists.cpp
@@ -11,41 +11,64 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {};    // Constructor with value and next
 };
 
+// Attempt 1 (Merge everything together)
+// auto mergeKLists(std::vector<ListNode*>& lists) -> ListNode* {
+//     std::priority_queue<int, std::vector<int>, std::greater<int>> queue;
+
+//     for (ListNode* node : lists) {
+//         ListNode* curr = node;
+//         while (node != nullptr) {
+//             queue.push(node->val);
+//             node = node->next;
+//         }
+//     }
+
+//     ListNode* dummy = new ListNode();
+//     ListNode* curr = dummy;
+
+//     while (not queue.empty()) {
+//         int value = queue.top();
+//         queue.pop();
+
+//         ListNode* new_node = new ListNode(value);
+//         curr->next = new_node;
+//         curr = new_node;
+//     }
+
+//     curr->next = nullptr;
+//     return dummy->next;
+// }
+
 auto mergeKLists(std::vector<ListNode*>& lists) -> ListNode* {
-    auto comp = [](ListNode* n1, ListNode* n2) {
-        return n1->val > n2->val;
+    auto cmp = [](ListNode* node1, ListNode* node2) {
+        return node1->val > node2->val;
     };
 
-    // Nothing too complicated, 
-    // Declaring a priority queue for ListNode,
-    // with container type of std::vector<ListNode>,
-    // with lambda function comp() as its custom comparator.
-    std::priority_queue<ListNode*, std::vector<ListNode*>, decltype(comp)> min_queue(comp);
+    // dammit what was decltype again?
+    std::priority_queue<ListNode*, std::vector<ListNode*>, decltype(cmp)> queue;
 
     for (ListNode* node : lists) {
-        if (node != nullptr) {
-            min_queue.push(node);
-        }
+        if (node != nullptr) queue.push(node);
     }
 
     ListNode* dummy = new ListNode();
-    ListNode* current = dummy;
+    ListNode* curr = dummy;
 
-    while (not min_queue.empty()) {
-        ListNode* smallest = min_queue.top();
-        min_queue.pop();
+    while (not queue.empty()) {
+        auto node = queue.top();
+        queue.pop();
 
-        current->next = smallest;
-        current = current->next;
+        curr->next = node;
+        curr = node;
 
-        if (smallest->next != nullptr) {
-            min_queue.push(smallest->next);
+        if (node->next != nullptr) {
+            queue.push(node->next);
         }
     }
 
     return dummy->next;
 }
 
-auto main() -> int {
+int main() {
     
 }
