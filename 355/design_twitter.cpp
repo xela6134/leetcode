cfcb98a7eef9020d3b9bd2872cb8fbf31ad8a802
xela6134 <leol3o05@gmail.com>
2025-03-07 00:51:31 +1100

    New solution for Design Twitter

--- 355/design_twitter.cpp
+++ 355/design_twitter.cpp
@@ -5,32 +5,47 @@
 #include <unordered_map>
 #include <utility>
 #include <algorithm>
+#include <queue>
 
 class Twitter {
 public:
     Twitter() {
-        
+        timestamp = 0;
     }
     
     void postTweet(int userId, int tweetId) {
-        tweets.push_back(std::make_pair(userId, tweetId));
+        tweets[userId].push(std::make_pair(timestamp, tweetId));
+        ++timestamp;
     }
     
     auto getNewsFeed(int userId) -> std::vector<int> {
-        std::vector<int> newsfeed;
-        int count = 0;
+        std::priority_queue<std::pair<int, int>> combined;
 
-        for (auto it = tweets.rbegin(); it != tweets.rend(); ++it) {
-            if (count >= 10) break;
-
-            auto [user_id, tweet_id] = *it;
-            if (user_id == userId || following[userId].contains(user_id)) {
-                newsfeed.push_back(tweet_id);
-                ++count;
+        if (tweets.find(userId) != tweets.end()) {
+            auto temp = tweets[userId];
+            while (!temp.empty()) {
+                combined.push(temp.top());
+                temp.pop();
             }
         }
 
-        return newsfeed;
+        for (auto followeeId : following[userId]) {
+            if (tweets.find(followeeId) != tweets.end()) {
+                auto temp = tweets[followeeId];
+                while (!temp.empty()) {
+                    combined.push(temp.top());
+                    temp.pop();
+                }
+            }
+        }
+
+        std::vector<int> feed;
+        while (not combined.empty() and feed.size() < 10) {
+            feed.push_back(combined.top().second);
+            combined.pop();
+        }
+        
+        return feed;
     }
     
     // FollowerID follows FolloweeID
@@ -43,9 +58,13 @@ public:
         following[followerId].erase(followeeId);
     }
 private:
-    // Stores all tweets. Stored from oldest to latest
-    std::vector<std::pair<int, int>> tweets;
+    // Timestamp
+    int timestamp;
 
     // Stores all following for a user
     std::unordered_map<int, std::unordered_set<int>> following;
+
+    // Stores tweets for a user
+    // [user]: { timestamp, tweetId }
+    std::unordered_map<int, std::priority_queue<std::pair<int, int>>> tweets;
 };
