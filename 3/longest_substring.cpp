6a960118b890c7f1a3f97d2128cef7e3a2c1a7fa
xela6134 <leol3o05@gmail.com>
2025-03-21 22:53:22 +1100

    Better solution for longest substring without repeating characters

--- 3/longest_substring.cpp
+++ 3/longest_substring.cpp
@@ -3,61 +3,30 @@
 #include <unordered_set>
 #include <algorithm>
 
-// auto lengthOfLongestSubstring(std::string s) -> int {
-//     int left = 0;
-//     int max_length = 0;
-//     auto char_set = std::unordered_set<char>{};
-
-//     for (int right = 0; right < s.size(); right++) {
-//         while (char_set.contains(s[right])) {
-//             char_set.erase(s[left]);
-//             left++;
-//         }
-
-//         char_set.insert(s[right]);
-//         max_length = std::max(max_length, right - left + 1);
-//     }
-
-//     return max_length;
-// }
-
-
-// p w w k e w
-// 1. 
-// 2. 
-// 3. 
-// 4. 
-// 5. 
-// 6. 
-// 7. 
-
-
 int lengthOfLongestSubstring(std::string s) {
-    int left = 0, right = 0;
-    int max_len = 0;
-    std::unordered_set<char> char_set;
+    std::unordered_set<char> characters;
+    int left = 0;
+    int max_length = 0;
 
-    while (right < s.size()) {
-        // std::cout << left << ", " << right << std::endl;
-
-        if (not char_set.contains(s[right])) {
-            // No duplicate encounter
-            
-            char_set.insert(s[right]);
-            max_len = std::max(max_len, right - left + 1);
-            ++right;
-        } else {
-            // Duplicate encounter - erase left, no counting max length for this one
-
-            char_set.erase(s[left]);
+    for (int right = 0; right < s.size(); ++right) {
+        // Increment left until duplicate elements are gone
+        while (characters.contains(s[right])) {
+            characters.erase(s[left]);
             ++left;
         }
+
+        characters.insert(s[right]);
+        max_length = std::max(max_length, right - left + 1);
     }
 
-    return max_len;
+    return max_length;
 }
 
 auto main() -> int {
-    auto str = std::string("pwwkew");
-    std::cout << lengthOfLongestSubstring(str) << std::endl;
+    std::string str1 = "abcabcbb";  // abc: 3
+    std::string str2 = "bbbbb";     // b  : 1
+    std::string str3 = "pwwkew";    // wke: 3
+    std::cout << lengthOfLongestSubstring(str1) << std::endl;
+    std::cout << lengthOfLongestSubstring(str2) << std::endl;
+    std::cout << lengthOfLongestSubstring(str3) << std::endl;
 }
