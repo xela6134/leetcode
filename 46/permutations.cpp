69f9b858fbcc81283cc06f7d849fbf0a6a7b350d
xela6134 <leol3o05@gmail.com>
2025-03-25 19:24:54 +1100

    Better solution for permutations (iterative)

--- 46/permutations.cpp
+++ 46/permutations.cpp
@@ -3,32 +3,26 @@
 #include <algorithm>
 #include <unordered_set>
 
-void generate(std::vector<int>& used, std::vector<int>& unused, std::vector<std::vector<int>>& result) {
-    if (unused.empty()) {
-        result.push_back(used);
-        return;
+std::vector<std::vector<int>> permute(std::vector<int>& nums) {
+    std::vector<std::vector<int>> perms = {{}};
+
+    // Initial: perms = {{}}
+    //   First: perms = {{1}}
+    //  Second: perms = {{1,2}, {2,1}}
+    //   Third: perms = {{3,2,1}, {2,3,1}, {2,1,3}, {3,1,2}, {1,3,2}, {1,2,3}}
+
+    for (int num : nums) {
+        std::vector<std::vector<int>> new_perms;
+        for (const auto& p : perms) {
+            for (int i = 0; i <= p.size(); i++) {
+                std::vector<int> p_copy = p;
+                p_copy.insert(p_copy.begin() + i, num);
+                new_perms.push_back(p_copy);
+            }
+        }
+        perms = new_perms;
     }
-
-    for (int num : unused) {
-        std::vector<int> new_used = used;
-        new_used.push_back(num);
-
-        std::vector<int> new_unused = unused;
-        new_unused.erase(std::remove(new_unused.begin(), new_unused.end(), num), new_unused.end());
-
-        generate(new_used, new_unused, result);
-    }
-}
-
-auto permute(std::vector<int>& nums) -> std::vector<std::vector<int>> {
-    std::vector<std::vector<int>> result;
-
-    std::vector<int> used;
-    std::vector<int> unused = nums;
-
-    generate(used, unused, result);
-
-    return result;
+    return perms;
 }
 
 auto print_result(std::vector<std::vector<int>> result) {
