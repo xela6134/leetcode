d79516aa5ee2e5b5118a412444c92d03e967ce79
xela6134 <leol3o05@gmail.com>
2025-04-19 13:15:11 +1000

    Solution for Best Time to Buy and Sell Stock (v2)

--- 121/buy_sell_stocks.cpp
+++ 121/buy_sell_stocks.cpp
@@ -2,18 +2,13 @@
 #include <iostream>
 #include <climits>
 
-// 7, 1, 5, 3, 6, 4
-// Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-
-// 7, 6, 4, 3, 1
-// In this case, no transactions are done and the max profit = 0
 int maxProfit(std::vector<int>& prices) {
+    int minimum = prices[0];
     int max_profit = 0;
-    int min_price = prices[0];
 
     for (int i = 1; i < prices.size(); ++i) {
-        max_profit = std::max(max_profit, prices[i] - min_price);
-        min_price = std::min(min_price, prices[1]);
+        minimum = std::min(minimum, prices[i]);
+        max_profit = std::max(max_profit, prices[i] - minimum);
     }
 
     return max_profit;
