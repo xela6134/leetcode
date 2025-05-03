348d8025fa792f7c4192bf28dc27115eb4dd0953
xela6134 <leol3o05@gmail.com>
2025-05-03 17:43:58 +0900

    Solution for Delete Leaves with Given Value

---
+++ 1325/delete_leaves_with_given_value.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "TreeNode.h"
+
+// We need post-order traversal for this
+void deletion(TreeNode*& root, int target) {
+    if (not root) return;
+
+    deletion(root->left,  target);
+    deletion(root->right, target);
+
+    // remove if turned into a target leaf
+    if (not root->left and not root->right and root->val == target) {
+        root = nullptr;
+    }
+}
+
+TreeNode* removeLeafNodes(TreeNode* root, int target) {
+    deletion(root, target);
+    return root;
+}
+
+int main() {
+
+}
