7d83f1c06e1f7399db28841a6a8495d793dec875
xela6134 <leol3o05@gmail.com>
2025-05-03 16:38:02 +0900

    Solution for Insert Into BST

--- 701/insert_into_bst.cpp
+++ 701/insert_into_bst.cpp
@@ -1,4 +1,42 @@
 #include <iostream>
+#include "TreeNode.h"
+
+void insert(TreeNode* node, int val, bool& found) {
+    if (not node or found) return;
+    
+    // Left side
+    if (node->val > val) {
+        if (not node->left) {
+            node->left = new TreeNode(val);
+            found = true;
+            return;
+        }
+        
+        insert(node->left, val, found);
+    }
+    
+    // Right side
+    else {
+        if (not node->right) {
+            node->right = new TreeNode(val);
+            found = true;
+            return;
+        }
+        
+        insert(node->right, val, found);
+    }
+}
+
+TreeNode* insertIntoBST(TreeNode* root, int val) {
+    bool found = false;
+
+    if (not root) {
+        return new TreeNode(val);
+    }
+
+    insert(root, val, found);
+    return root;
+}
 
 int main() {
 
