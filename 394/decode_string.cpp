b178e79213924ca6ee8b694085340bde3779f030
xela6134 <leol3o05@gmail.com>
2025-05-21 16:26:15 +0900

    Solution for Decode String

--- 394/decode_string.cpp
+++ 394/decode_string.cpp
@@ -1,17 +1,48 @@
+#include <iostream>
 #include <string>
 #include <stack>
-#include <utility>
-#include <algorithm>
-#include <iostream>
 
-auto decodeString(std::string s) -> std::string {
-    // 3[a]2[bc]
-    // 3[a2[c]]
-    // 2[abc]3[cd]ef
+std::string decodeString(std::string s) {
+    std::stack<int> count_stack;
+    std::stack<std::string> str_stack;
 
-    return "";
+    std::string result;
+    int num = 0;
+
+    for (char c : s) {
+        // Keep adding numbers
+        if (isdigit(c)) {
+            num = num * 10 + (c - '0');
+        } 
+        // Start of string
+        else if (c == '[') {
+            count_stack.push(num);
+            str_stack.push(result);
+            num = 0;
+            result = "";
+        } 
+        // End of string
+        else if (c == ']') {
+            int repeat = count_stack.top(); 
+            count_stack.pop();
+            std::string prev = str_stack.top(); 
+            str_stack.pop();
+            
+            for (int i = 0; i < repeat; ++i) {
+                prev += result;
+            }
+
+            result = prev;
+        } 
+        // Plain chars
+        else {
+            result += c;
+        }
+    }
+
+    return result;
 }
 
-auto main() -> int {
-    std::cout << decodeString("s");
+int main() {
+
 }
