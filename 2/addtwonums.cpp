229d8ff9e3a6aeb6b2010976dd1151f4ebab3113
xela6134 <leol3o05@gmail.com>
2025-05-24 13:34:45 +0900

    Solution for Add Two Numbers

--- 2/addtwonums.cpp
+++ 2/addtwonums.cpp
@@ -19,25 +19,14 @@
 // add remainder in next iteration
 
 #include <iostream>
+#include "ListNode.h"
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
-
-auto addTwoNumbers(ListNode* l1, ListNode* l2) -> ListNode* {
-    // Revision: head is only made to initialise, meaningful values are stored afterwards
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
     ListNode* head = new ListNode();
     ListNode* curr = head;
 
     int carry = 0;
-
-    // Revision: (l1 || l2 || carry) syntax works in C++, no more C
-    while (l1 || l2 || carry) {
+    while (l1 or l2 or carry) {
         int sum = carry;
 
         if (l1) {
@@ -60,18 +49,9 @@ auto addTwoNumbers(ListNode* l1, ListNode* l2) -> ListNode* {
     return head->next;
 }
 
-void printList(ListNode* head) {
-    while (head) {
-        std::cout << head->val << " ";
-        head = head->next;
-    }
-    std::cout << std::endl;
-}
-
-auto main() -> int {
+int main() {
     ListNode* l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
     ListNode* l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
 
     ListNode* list = addTwoNumbers(l1, l2);
-    printList(list);
 }
