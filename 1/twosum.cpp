71396f7a11a6c8173b3aa7af6eefa68f1d938be0
xela6134 <leol3o05@gmail.com>
2025-05-25 16:54:10 +0900

    Solution for Two Sum

--- 1/twosum.cpp
+++ 1/twosum.cpp
@@ -16,21 +16,26 @@
 //     return {};
 // }
 
-auto twoSum(std::vector<int>& nums, int target) -> std::vector<int> {
-    std::unordered_map<int, int> num_map;
-
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> seen;
+    
+    // 1. Check for target - num
+    // 2. If not exists, insert as { num, index }
+    // 3. If exists, return the indexes
     for (int i = 0; i < nums.size(); ++i) {
-        if (num_map.contains(target - nums[i])) {
-            return {num_map[target - nums[i]], i};
+        int num = nums[i];
+
+        if (seen.find(target - num) == seen.end()) {
+            seen[num] = i;
         } else {
-            num_map[nums[i]] = i;
+            return {seen[target - num], i};
         }
     }
 
     return {};
 }
 
-auto main() -> int {
+int main() {
     std::vector<int> nums = {2, 7, 11, 15};
     int target = 9;
     auto result = twoSum(nums, target);
