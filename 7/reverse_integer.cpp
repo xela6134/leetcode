fc8e91fd7da015866e1220e4acac27114be29695
xela6134 <leol3o05@gmail.com>
2025-05-29 16:00:14 +0900

    Solution for Reverse Integer

--- 7/reverse_integer.cpp
+++ 7/reverse_integer.cpp
@@ -2,32 +2,20 @@
 #include <climits>
 
 int reverse(int x) {
-    bool reversed = false;
-    long y = static_cast<long>(x);
-    if (y < 0) {
-        reversed = true;
-        y *= -1;
+    int rev = 0;
+
+    while (x != 0) {
+        int pop = x % 10;
+        x /= 10;
+
+        // Check for overflow before pushing the digit
+        if (rev > (INT_MAX / 10)) return 0;
+        if (rev < (INT_MIN / 10)) return 0;
+
+        rev = rev * 10 + pop;
     }
 
-    // 123 % 10 = 3
-    // 12 % 10 = 2
-    // 1 % 10 = 1
-    
-    long y_reversed = 0;
-    int n = y;
-
-    while (n > 0) {
-        y_reversed = y_reversed * 10 + (n % 10);
-        n /= 10;
-    }
-
-    if (reversed) y_reversed *= -1;
-
-    if (INT_MIN <= y_reversed and y_reversed <= INT_MAX) {
-        return static_cast<int>(y_reversed);
-    } else {
-        return 0;
-    }
+    return rev;
 }
 
 int main() {
