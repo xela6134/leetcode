fb964be153acc6fd973ba648745137ced5caaa13
xela6134 <leol3o05@gmail.com>
2025-05-29 23:36:43 +0900

    Solution for Excel Sheet Column Number

---
+++ 171/excel_sheet_column_number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int titleToNumber(std::string columnTitle) {
+    // ZY = (Z - A) * 26 + (Y - A) * 1
+    int curr = 0;
+    for (char c : columnTitle) {
+        curr = curr * 26 + (c - 'A' + 1);
+    }
+
+    return curr;
+}
+
+int main() {
+
+}
